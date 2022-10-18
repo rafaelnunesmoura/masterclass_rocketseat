c465673cf4fda5e0258cb4e7208c9983880eb8f5
Rafael <rafaelnunesmoura1@protonmail.com>
2022-10-18 00:23:08 -0300

    end api

---
+++ web_server/api/index.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const URL = require('url')
+const data = require('./urls.json')
+const fs = require('fs')
+const path = require('path')
+
+function writeFile(cb) {
+  return fs.writeFile(
+    path.join(__dirname,"urls.json"), 
+    JSON.stringify(data, null, 2 ),
+    err => {
+      if(err) throw err
+
+      cb(JSON.stringify({message: "Tudo certo!"}))
+    }
+  )
+}
+
+http.createServer((req, res) => {
+  const {name, url, del} = URL.parse(req.url,true).query
+
+  res.writeHead(200, {
+    'Access-Control-Allow-Origin': '*'
+  })
+
+  //all resources
+  if(!name|| !url)
+    return res.end('show all')
+
+  if(del) {
+    data.url = data.url.filter(item => String(item.url) !== String(url))
+
+    return writeFile((message) => res.end(message))
+
+  }
+
+  data.url.push({name, url})
+  return writeFile((message) => res.end(message))
+
+}).listen(3000, () => console.log('api is Running'))
